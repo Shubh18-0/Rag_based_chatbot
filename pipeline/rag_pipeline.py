4f075960e3d6557334b9e83f1d1146d8b13fa3f8
Shubh18-0 <bhardwajshubham788@gmail.com>
2025-10-30 11:11:36 +0530

    updated code

--- pipeline/rag_pipeline.py
+++ pipeline/rag_pipeline.py
@@ -10,7 +10,7 @@ from pinecone_text.sparse import BM25Encoder
 from langchain_community.vectorstores import Pinecone as Pineconevectorstore
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables import RunnableWithMessageHistory
-from langchain.chains.combine_documents.stuff import create_stuff_documents_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.history_aware_retriever import create_history_aware_retriever
 
