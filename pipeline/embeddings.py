0c24c4c1ba7e551c0d7bfd743541947bf059143b
Shubh18-0 <bhardwajshubham788@gmail.com>
2025-11-21 13:38:09 +0530

    updated

--- pipeline/embeddings.py
+++ pipeline/embeddings.py
@@ -1,4 +1,4 @@
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings
 
 def create_embeddings():
     return HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
